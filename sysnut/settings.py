ac8c92973336033f83e97f30ea3411fc31f0e38d
Linneu Lopes <Linneu.DM@Gmail.com>
2018-02-19 23:13:10 -0300

    Add ajax food substitute

--- sysnut/settings.py
+++ sysnut/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'core')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #print(STATIC_ROOT)
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
