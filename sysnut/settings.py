5778e0eeb224c91880263548d4809700df8b4604
Linneu Lopes <Linneu.DM@Gmail.com>
2018-02-20 00:04:38 -0300

    Add ajax food substitute

--- sysnut/settings.py
+++ sysnut/settings.py
@@ -180,10 +180,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+STATIC_URL = '/static/'
+
 
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
